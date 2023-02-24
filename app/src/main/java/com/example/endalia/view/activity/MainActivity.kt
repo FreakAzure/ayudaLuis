15a266ac10d15d77887b8a76b91a29a9924b9c6a
Luis Cros <luiscros98@gmail.com>
2023-02-24 14:55:59 +0100

    vista_and_nav

--- app/src/main/java/com/example/endalia/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/endalia/view/activity/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.endalia.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.commit
 import com.example.endalia.R
 import com.example.endalia.util.Singleton
+import com.example.endalia.view.fragment.login.LoginFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,9 +14,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (Singleton.employee != null) {
-            // Navegar a Home
-        } else {
-            // Navegar a login
+            // Navegar a home
+            }
+        else{
+            supportFragmentManager.beginTransaction().replace(R.id.nav_login,LoginFragment()).commit()
         }
     }
 }
