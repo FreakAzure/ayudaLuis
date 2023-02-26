7ce56be61ef648009db1d52a58cd31c1e7b5f05b
lcros-mo <luiscros98@gmail.com>
2023-02-26 21:01:10 +0100

    views and nav

--- app/src/main/java/com/example/endalia/view/fragment/register/RegisterFragment.kt
+++ app/src/main/java/com/example/endalia/view/fragment/register/RegisterFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.ViewModelProvider
 import com.example.endalia.R
+import com.example.endalia.view.fragment.register.RegisterViewModel
+import com.google.android.material.textfield.TextInputEditText
 
 class RegisterFragment : Fragment() {
+
+    private lateinit var viewModel: RegisterViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,4 +24,66 @@ class RegisterFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[RegisterViewModel::class.java]
+        viewModel.configViewmodel(requireContext())
+        observeViewModel()
+        setListeners()
+
+    }
+    private fun setListeners() {
+        val button = view?.findViewById<Button>(R.id.registerButton)
+        val emailInput = view?.findViewById<TextInputEditText>(R.id.userEmail)
+        val passInput = view?.findViewById<TextInputEditText>(R.id.userPass)
+        val confirmPassInput = view?.findViewById<TextInputEditText>(R.id.reUserPass)
+
+        button?.setOnClickListener {
+            viewModel.performRegister()
+        }
+
+        emailInput?.doOnTextChanged { text, _, _, _ ->
+            // Updates viewmodel _userEmail
+            viewModel.setUserEmail(text.toString())
+        }
+
+        passInput?.doOnTextChanged { text, _, _, _ ->
+            // Updates viewmodel _userPass
+            viewModel.setUserPass(text.toString())
+        }
+
+        confirmPassInput?.doOnTextChanged { text, _, _, _ ->
+            // Updates viewmodel _confirmUserPass
+            viewModel.setConfirmPass(text.toString())
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.registerSuccess.observe(viewLifecycleOwner) {
+            when (it) {
+                RegisterViewModel.RegisterState.EmailError -> {
+                    // Show error on email field
+                }
+                RegisterViewModel.RegisterState.PasswordError -> {
+                    // Show error on pass field
+                }
+                RegisterViewModel.RegisterState.ConfirmPasswordError -> {
+                    // Show error on confirm pass field
+                }
+                RegisterViewModel.RegisterState.BothError -> {
+                    // Show error on both fields
+                }
+                RegisterViewModel.RegisterState.None -> {
+                    // Hide error on all fields
+                }
+                RegisterViewModel.RegisterState.UserAlreadyExistsError -> {
+                    // Show user already exists error
+                }
+                RegisterViewModel.RegisterState.Success -> {
+                    // Registration successful
+                }
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/endalia/view/fragment/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/endalia/view/fragment/register/RegisterViewModel.kt
@@ -1,4 +1,100 @@
 package com.example.endalia.view.fragment.register
 
-class RegisterViewModel {
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.room.Room
+import com.example.endalia.models.Empleado
+import com.example.endalia.controllers.AppDatabase
+
+class RegisterViewModel : ViewModel() {
+
+    private lateinit var _database: AppDatabase
+    private val _userEmail = MutableLiveData<String>()
+    private val _userPass = MutableLiveData<String>()
+    private val _confirmUserPass = MutableLiveData<String>()
+    private val _registerSuccess = MutableLiveData<RegisterState>()
+
+
+    enum class RegisterState  {
+        EmailError,
+        PasswordError,
+        ConfirmPasswordError,
+        BothError,
+        None,
+        Success,
+        UserAlreadyExistsError
+    }
+    val userEmail: LiveData<String> = _userEmail
+    val userPass: LiveData<String> = _userPass
+    val confirmPass: LiveData<String> = _confirmUserPass
+    val registerSuccess: LiveData<RegisterState> = _registerSuccess
+
+    fun configViewmodel(context: Context) {
+        _database = setupDb(context)
+    }
+
+    fun setUserEmail(email: String) {
+        _userEmail.value = email
+    }
+
+    fun setUserPass(password: String) {
+        _userPass.value = password
+    }
+
+    fun setConfirmPass(password: String) {
+        _confirmUserPass.value = password
+    }
+
+    fun buttonClicks() {
+
+    }
+
+    fun performRegister() {
+        if (!fieldsCorrect()) {
+            return
+        }
+        val empleado = _database.empleadoDao().findEmployeeByEmail(_userEmail.value!!)
+        if (empleado != null) {
+            this._registerSuccess.value = RegisterState.UserAlreadyExistsError
+            return
+        }
+        val newEmpleado = Empleado(correo = _userEmail.value!!, contraseña = _userPass.value!!)
+        _database.empleadoDao().insert(newEmpleado)
+        this._registerSuccess.value = RegisterState.Success
+    }
+    private fun setupDb(context: Context): AppDatabase {
+        return  Room.databaseBuilder(
+            context,
+            AppDatabase::class.java, "endalia-db"
+        ).build()
+    }
+    private fun fieldsCorrect(): Boolean {
+        if (_userEmail.value == null || _userPass.value == null || _confirmUserPass.value == null) {
+            this._registerSuccess.value = RegisterState.BothError
+            return false
+        }
+        if (!isValidEmail(_userEmail.value!!)) {
+            this._registerSuccess.value = RegisterState.EmailError
+            return false
+        }
+        if (!isValidPassword(_userPass.value!!)) {
+            this._registerSuccess.value = RegisterState.PasswordError
+            return false
+        }
+        if (_userPass.value != _confirmUserPass.value) {
+            this._registerSuccess.value = RegisterState.ConfirmPasswordError
+            return false
+        }
+        this._registerSuccess.value = RegisterState.None
+        return true
+    }
+    private fun isValidEmail(email: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    private fun isValidPassword(password: String): Boolean {
+        return password.length >= 6
+    }
 }
